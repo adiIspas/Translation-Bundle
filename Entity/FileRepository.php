2746a670500500d9b19d27bf243d1d889c568fe1
Adrian Ispas <adi.ispas95@gmail.com>
2016-08-16 17:15:58 +0300

    Some changes ...

--- Entity/FileRepository.php
+++ Entity/FileRepository.php
@@ -3,6 +3,10 @@
 namespace Lexik\Bundle\TranslationBundle\Entity;
 
 use Doctrine\ORM\EntityRepository;
+use Guzzle\Http\Exception\BadResponseException;
+use Guzzle\Http\Message\Header;
+use Guzzle\Http\Message\Response;
+use Guzzle\Service\Client;
 
 /**
  * Repository for TransUnit entity.
@@ -20,16 +24,71 @@ class FileRepository extends EntityRepository
      */
     public function findForLocalesAndDomains(array $locales, array $domains)
     {
-        $builder = $this->createQueryBuilder('f');
+//        $builder = $this->createQueryBuilder('f');
+//
+//        if (count($locales) > 0) {
+//            $builder->andWhere($builder->expr()->in('f.locale', $locales));
+//        }
+//
+//        if (count($domains) > 0) {
+//            $builder->andWhere($builder->expr()->in('f.domain', $domains));
+//        }
+//
+//        $files = $builder->getQuery()->getResult();
 
-        if (count($locales) > 0) {
-            $builder->andWhere($builder->expr()->in('f.locale', $locales));
+        $method = 'POST';
+        $uri = 'http://localhost:8080/app_dev.php/api/find_for_locales_and_domains';
+        $body['locales'] = $locales;
+        $body['domains'] = $domains;
+
+        $responseFind = $this->getResponseFromUrl($method, $uri);
+        $results = json_decode($responseFind->getBody(true), true);
+
+        $files = array();
+        foreach ($results as $result) {
+            $file = new File();
+
+            $file->setId($result['id']);
+            $file->setLocale($result['locale']);
+            $file->setDomain($result['domain']);
+            $file->setHash($result['hash']);
+            $file->setExtention($result['extention']);
+            $file->setPath($result['path']);
+
+            $files[] = $file;
         }
 
-        if (count($domains) > 0) {
-            $builder->andWhere($builder->expr()->in('f.domain', $domains));
+        return $files;
+    }
+
+    private function getResponseFromUrl($method, $uri, $headers = null, $body = null, $options = array())
+    {
+        $client = new Client();
+
+        try {
+            /** @var Response $response */
+            $response = $client->createRequest(
+                $method,
+                $uri,
+                $headers,
+                $body,
+                $options
+            )->send();
+        } catch (BadResponseException $e) {
+            $response = $e->getResponse();
+            $request  = $e->getRequest();
+            if ($response instanceof Response) {
+                $message = json_decode($response->getBody(true), true);
+                if (isset($message['errors'])) {
+                    $ex = new AuthClientErrorResponseException(key($message['errors']));
+                    $ex->setResponse($response);
+                    $ex->setRequest($request);
+                    throw $ex;
+                }
+            }
+            throw $e;
         }
 
-        return $builder->getQuery()->getResult();
+        return $response;
     }
 }
